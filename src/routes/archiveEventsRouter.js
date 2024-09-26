2650f187cd1f8fcfabf8ac50e72ef5105a65a444
JonasTiago <jonastiago95@gmail.com>
2024-09-26 12:18:16 -0300

    fix: router events

--- src/routes/archiveEventsRouter.js
+++ src/routes/archiveEventsRouter.js
@@ -16,8 +16,10 @@ const archiveEventsRoutes = new Router();
 
 archiveEventsRoutes.get("/", listContents);
 archiveEventsRoutes.get("/detectors", listEvents);
+
 archiveEventsRoutes.get("/:starttime", archiveStream);
 archiveEventsRoutes.get("/stop/:uuid", stopStream);
+
 archiveEventsRoutes.get("/frame/snapshot/:starttime", getFrame);
 archiveEventsRoutes.get("/frames/:endtime/:begintime", listFramesByVideo);
 
